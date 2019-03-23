a71b4a8bcae41f66271daa4050415fbf5c0f84a2
Nicola Romani <nicola.romani89@gmail.com>
2019-03-24 00:03:07 +0100

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Main from "./components/containers/Main";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 class App extends Component {
   render() {
