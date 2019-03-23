bfae6be012c901dc63a518bb5c501bc86d848606
Nicola Romani <nicola.romani89@gmail.com>
2019-03-23 22:12:14 +0100

    chore: bug fixes

--- client/src/components/containers/Main.js
+++ client/src/components/containers/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Home from "../screens/Home";
+import NavbarMenu from "../interface/NavbarMenu";
 
 export default class Main extends Component {
   render() {
     return (
       <div className="container">
+        <NavbarMenu />
         <Home />
       </div>
     );
