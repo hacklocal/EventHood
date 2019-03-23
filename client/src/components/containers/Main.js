e29c32d2d935c7d25a4bdea7a893c7e444f8e132
Raffaele Tufano <pocho962010@hotmail.it>
2019-03-24 00:02:10 +0100

    Aggiunte modifiche componente Home

--- client/src/components/containers/Main.js
+++ client/src/components/containers/Main.js
@@ -5,7 +5,7 @@ import NavbarMenu from "../interface/NavbarMenu";
 export default class Main extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <NavbarMenu />
         <Home />
       </div>
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import logo from "../../assets/images/logo22.png";
-import { Alert } from "react-bootstrap"
+import { Form, Button, Col, Row } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="home-container">
-        <div className="logo">
-          <img src={logo} alt="logo" height={200} align="middle" />>
+      <div className="container">
+        <div id="logo">
+          <img src={logo} alt="logo" width={400} height={100} align="middle" />
         </div>
-        <button className="primary-button">TROVA EVENTI</button>
+
+        <div id="formSearch">
+          <Form>
+            <Row className="justify-content-md-center">
+              <Col sm={12} md={6} lg={6}>
+                <Form.Group controlId="inputSearch">
+                  <Form.Control type="text" placeholder="Cerca Evento..." />
+                </Form.Group>
+                <Button variant="light" type="submit">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
       </div>
     );
   }
