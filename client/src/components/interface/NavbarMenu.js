253990d3e5d576c15611114ad64a63cc268c460a
Raffaele Tufano <pocho962010@hotmail.it>
2019-03-24 05:22:19 +0100

    modding

--- client/src/components/interface/NavbarMenu.js
+++ client/src/components/interface/NavbarMenu.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button
-} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 export default class NavbarMenu extends Component {
   render() {
@@ -17,7 +10,6 @@ export default class NavbarMenu extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logo from "../../assets/images/logo22.png";
-import { Form, Button, Col, Row } from "react-bootstrap";
+import { Form, Col, Row } from "react-bootstrap";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default class Home extends Component {
   render() {
@@ -14,12 +16,20 @@ export default class Home extends Component {
           <Form>
             <Row className="justify-content-md-center">
               <Col sm={12} md={6} lg={6}>
-                <Form.Group controlId="inputSearch">
+                <Form.Group controlId="formGridState">
+                  <Form.Label>Seleziona un quartiere</Form.Label>
+                  <Form.Control as="select">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+                {/*<Form.Group controlId="inputSearch">
                   <Form.Control type="text" placeholder="Cerca Evento..." />
                 </Form.Group>
-                <Button variant="light" type="submit">
-                  Submit
+                <Button id="btnSearch" variant="light" type="submit">
+                  <FontAwesomeIcon icon={faSearch} size="lg" />
                 </Button>
+                */}
               </Col>
             </Row>
           </Form>
